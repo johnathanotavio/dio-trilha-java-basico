65d0dba39d66b576396dddada87a29f3f1224ec2
Johnathan <johnathan_omdc@outlook.com>
2023-10-06 00:49:46 -0300

    Operações incremento e decrementar de numeros

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,13 +3,21 @@ public class Operadores {
   public static void main(String[] args) {
     int numero = 5;
 
-    numero = - numero;
+    //x repetição 
+    
+    //numero = numero +1;
+
+    System.out.println( numero --);
 
     System.out.println(numero);
 
-    numero = numero * -1;
+    //negando expressões booolean
 
-    System.out.println(numero);
+    boolean variavel = true;
+
+    variavel = !variavel;
+
+    System.out.println(variavel);
   }
   
 }
